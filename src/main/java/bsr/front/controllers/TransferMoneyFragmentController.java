7993f8397960eba268b733acdb44aa7b707adcdd
impresyjna <joanna.furmaniak@student.put.poznan.pl>
2017-01-18 21:12:18 +0100

    Transfer ready

---
+++ src/main/java/bsr/front/controllers/TransferMoneyFragmentController.java
@@ -0,0 +1,72 @@
+package bsr.front.controllers;
+
+import bsr.front.models.AccountChoiceBoxModel;
+import bsr.front.singletonInstances.ServerConnection;
+import bsr.front.utils.DialogsUtil;
+import bsr.server.innerservices.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.util.List;
+
+/**
+ * Created by Impresyjna on 18.01.2017.
+ */
+public class TransferMoneyFragmentController {
+    @FXML
+    private javafx.scene.control.ChoiceBox<AccountChoiceBoxModel> accountChoiceBox;
+    @FXML
+    private TextField amountTextField;
+    @FXML
+    private TextField titleTextField;
+    @FXML
+    private TextField targetTextField;
+
+    @FXML
+    public void initialize() {
+        getAccountsFromServer();
+    }
+
+    @FXML
+    public void transferAction(ActionEvent event) {
+        String selectedBankAccountNumber = accountChoiceBox.getValue().getKey();
+        String amount = amountTextField.getText();
+        String title = titleTextField.getText();
+        String targetAccountNumber = targetTextField.getText();
+
+        try {
+            Operation newOperation = null;
+            newOperation = ServerConnection.getInstance().getAccountService().transferMoney(title, amount, selectedBankAccountNumber, targetAccountNumber);
+            DialogsUtil.showSuccess("Transfer succeeded \n Account balance: " + (double) newOperation.getBalanceAfter()/100 + "$");
+            clearFormAndRefresh();
+        } catch (AccountServiceException_Exception | NotValidException_Exception | OperationException_Exception | SessionException_Exception | AccountException_Exception | UserException_Exception | IOException_Exception  e) {
+            DialogsUtil.showException(e.getMessage());
+        }
+    }
+
+    private void clearFormAndRefresh() {
+        accountChoiceBox.valueProperty().set(null);
+        amountTextField.clear();
+        titleTextField.clear();
+        targetTextField.clear();
+        getAccountsFromServer();
+    }
+
+    private void getAccountsFromServer() {
+        accountChoiceBox.getItems().clear();
+        ServerConnection serverConnection = ServerConnection.getInstance();
+        try {
+            List<Account> accountsList = serverConnection.getAccountService().getAccounts();
+            ObservableList<AccountChoiceBoxModel> accountModels = FXCollections.observableArrayList();
+            for (Account account : accountsList) {
+                accountModels.add(new AccountChoiceBoxModel(account.getAccountNumber(), "Name: " + account.getTitleOFAccount() + "\nNumber: " + account.getAccountNumber() + "\nBalance: " + (double) account.getBalance() / 100 + " $"));
+            }
+            accountChoiceBox.setItems(accountModels);
+        } catch (SessionException_Exception | UserException_Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
